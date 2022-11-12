f7af0ae831a77303473649ecce1699546ae28b47
kevin_Arpe <arpeggiogio@gmail.com>
2022-11-12 17:48:14 +0900

    Project Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderView></HeaderView>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderView from './views/HeaderView.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+      HeaderView,
+    }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="header">
+    <div class="side_nav"></div>
+    <div class="nav">
+        <div class="container_topNav"></div>
+        <div>
+            <input class='toggle' type="checkbox">
+            <label for="toggle" class="cont"></label>
+        </div>
+        <i class="fa-regular fa-moon"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderView',
+}
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 80px;
+    background-color: #1c4769;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+}
+
+.nav {
+    display: flex;
+    align-items: center;
+}
+
+.fa-moon {
+    font-size: 1.4rem;
+    color: #fff;
+    margin: 20px;
+}
+
+.cont {
+  height: 20px;
+  width: 40px;
+  background: #202430;
+  border-radius: 80px;
+  overflow: hidden;
+  cursor: pointer;
+
+  transition: 0.3s;
+  display: flex;
+  align-items: center;
+}
+
+input {
+  display: none;
+}
+
+.toggle:checked ~ .cont {
+  background: whitesmoke;
+  box-shadow: 0 0 0 1200px #202430;
+}
+
+.cont:before {
+  content: "";
+  margin: 0 10px;
+  height: 20px;
+  width: 20px;
+  background: whitesmoke;
+  border-radius: 80px;
+  transition: 0.5s;
+}
+
+.toggle:checked ~ .cont:before {
+  transform: translateX(112%);
+  background: #202430;
+}
+</style>
